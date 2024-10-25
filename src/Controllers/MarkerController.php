5082fc3e9ef361df1584441041abf5fa31b13fd0
Veneridze <viacheslav.kopaev@yandex.ru>
2024-10-25 22:25:03 +0300

    Update MarkerController.php

--- src/Controllers/MarkerController.php
+++ src/Controllers/MarkerController.php
@@ -1,7 +1,9 @@
 <?php
 namespace Veneridze\LaravelMarker\Controllers;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Routing\ResponseFactory;
 use Veneridze\LaravelMarker\Models\Marker;
 use Veneridze\LaravelMarker\Models\ModelMarker;
 use Veneridze\LaravelMarker\Requests\MarkerRequest;
@@ -10,10 +12,30 @@ use Veneridze\LaravelMarker\Interfaces\WithMarksInterface;
 class MarkerController extends \Illuminate\Routing\Controller
 {
 
-    public function store(WithMarksInterface $model, Marker $marker, MarkerRequest $request): RedirectResponse
+    public function store(WithMarksInterface $model, Marker $marker, MarkerRequest $request)
     {
-        $model->addMarker($marker, $request->input('year'),$request->input('month'),$request->input('day'),$request->input('comment'),);
-        return back();
+        $mark = $model->addMarker($marker, $request->input('year'),$request->input('month'),$request->input('day'),$request->input('comment'),);
+        return [
+            "id" => $mark->id,
+            "value" => $mark->name,
+            "comment" => $mark->comment,
+            "user" => [
+                "id" => 1,
+                "name" => "Петров"
+            ],
+            "created_at" => $mark->created_at->format('Y-m-d')
+        ];
+    }
+
+    public function flushAllDayMarks(WithMarksInterface $model, MarkerRequest $request): Response|ResponseFactory {
+        $model->markers()->where([
+            'year' => $request->input('year'),
+            'month' => $request->input('month'),
+            'day' => $request->input('day'),
+        ])->delete();
+        return response(null, 204);
+        //$mark = $model->addMarker($marker, $request->input('year'),$request->input('month'),$request->input('day'),$request->input('comment'),);
+        
     }
 
     public function destroy(Marker $marker): RedirectResponse
